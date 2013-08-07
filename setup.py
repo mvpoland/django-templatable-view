cc8cf5e7a1338d2a3b4f5526856fc63dd235ba5a
Niels Van Och <niels.vanoch@gmail.com>
2013-08-07 11:00:35 +0200

    Correct wrong package path

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     description = "Decorator which takes care of rendering the response in Django views.",
     long_description = open('README.rst','r').read(),
     author = 'Jonathan Slenders, City Live nv',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
+    packages = find_packages('templatable_view'),
+    package_dir = {'': 'templatable_view'},
     classifiers = [
         'Programming Language :: Python',
         'Operating System :: OS Independent',
